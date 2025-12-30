0c4fcb12f51c9dfdda537f70ed3e5b296d86cda3
Yujin <wjd2000db@gmail.com>
2025-12-30 11:52:29 +0900

    style: #35 - 주문서 모달 css 수정

--- src/components/order/OrderPrintModal.vue
+++ src/components/order/OrderPrintModal.vue
@@ -17,27 +17,29 @@
           id="printable-area"
           class="w-[210mm] min-h-[297mm] bg-white p-[15mm] text-black shadow-lg"
         >          
-          <h1 class="text-4xl font-extrabold text-center tracking-[15px] mb-12 border-b-[3px] border-black pb-4">주 문 서</h1>
+          <h1 class="text-4xl font-medium text-center tracking-[15px] mb-12 border-b-[2px] border-black pb-4">주 문 서</h1>
 
           <div class="grid grid-cols-2 gap-0 border-t border-l border-black mb-8" style="display: flex;">
             <div class="border-r border-b border-black" style="flex: 1;">
               <div class="bg-gray-100 p-2 font-bold border-b border-black text-center">발주처</div>
-              <table class="w-full text-sm">
-                <tr><td class="w-24 bg-gray-50 p-2 border-r border-black font-bold text-center">회사명</td><td class="p-2">{{ order.clientName }}</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">담당자</td><td class="p-2">{{ order.clientManagerName }}</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">연락처</td><td class="p-2">{{ order.clientManagerContact }}</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">주소</td><td class="p-2 leading-tight">{{ order.clientAddress || '-' }}</td></tr>
-              </table>
-            </div>
+                <table class="w-full text-sm">
+                  <tbody> <tr><td class="w-24 bg-gray-50 p-2 border-r border-black font-bold text-center">회사명</td><td class="p-2">{{ order.clientName }}</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">담당자</td><td class="p-2">{{ order.clientManagerName }}</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">연락처</td><td class="p-2">{{ order.clientManagerContact }}</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">주소</td><td class="p-2 leading-tight">{{ order.clientAddress || '-' }}</td></tr>
+                  </tbody> </table>
+              </div>
+
             <div class="border-r border-b border-black" style="flex: 1;">
               <div class="bg-gray-100 p-2 font-bold border-b border-black text-center">수주처</div>
-              <table class="w-full text-sm">
-                <tr><td class="w-24 bg-gray-50 p-2 border-r border-black font-bold text-center">회사명</td><td class="p-2">새로</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">담당자</td><td class="p-2">{{ order.managerName || '-' }}</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">연락처</td><td class="p-2">010-4444-5555</td></tr>
-                <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">주소</td><td class="p-2 leading-tight">서울시 동작구 보라매로 87</td></tr>
-              </table>
-            </div>
+                <table class="w-full text-sm">
+                  <tbody> <tr><td class="w-24 bg-gray-50 p-2 border-r border-black font-bold text-center">회사명</td><td class="p-2">새로</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">담당자</td><td class="p-2">{{ order.managerName || '-' }}</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">연락처</td><td class="p-2">010-4444-5555</td></tr>
+                    <tr class="border-t border-black"><td class="bg-gray-50 p-2 border-r border-black font-bold text-center">주소</td><td class="p-2 leading-tight">서울시 동작구 보라매로 87</td></tr>
+                  </tbody> 
+                </table>
+              </div>
           </div>
 
           <div class="border-t border-l border-black mb-8 text-sm" style="display: grid; grid-template-columns: repeat(4, 1fr);">
@@ -88,13 +90,13 @@
           </table>
 
           <div class="border-t border-l border-black text-sm font-medium" style="display: grid; grid-template-columns: repeat(4, 1fr);">
-            <div class="bg-gray-100 p-3 border-r border-b border-black text-center">총 수량</div>
+            <div class="bg-gray-100 p-3 border-r border-b border-black font-bold text-center">총 수량</div>
             <div class="p-3 border-r border-b border-black text-center">{{ formatNumber(totalQuantity) }}</div>
-            <div class="bg-gray-100 p-3 border-r border-b border-black text-center">공급가액</div>
+            <div class="bg-gray-100 p-3 border-r border-b font-bold border-black text-center">공급가액</div>
             <div class="p-3 border-r border-b border-black text-right">{{ formatNumber(order.totalPrice) }} 원</div>
             
             <div class="bg-gray-100 p-4 border-r border-b border-black text-center flex items-center justify-center">총 합계 금액</div>
-            <div class="p-4 border-r border-b border-black text-right" style="grid-column: span 3;">
+            <div class="p-4 border-r border-b font-bold border-black text-right" style="grid-column: span 3;">
               {{ formatNumber(order.totalPrice) }} 원
             </div>
           </div>
@@ -102,9 +104,6 @@
       </div>
 
       <div class="px-6 py-4 border-t bg-gray-50 flex justify-end gap-3">
-        <button @click="$emit('close')" class="px-6 py-2 rounded-lg border border-gray-300 bg-white font-bold text-gray-700 hover:bg-gray-100">
-          닫기
-        </button>
         <button @click="handlePrint" class="px-8 py-2 rounded-lg bg-[#4C4CDD] font-bold text-white hover:bg-[#3b3bbb] shadow-md transition-all active:scale-95">
           인쇄
         </button>
